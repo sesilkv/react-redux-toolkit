a2fd58875714ce50cd3144dfa47d2045e71e5a01
KIRANA\kirana <kirana0701@gmail.com>
2022-04-21 16:45:18 +0700

    fetch-API-product

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import counterSlice from './store/counter'
 import Burger from './Components/Burger/Burger'
+import Product from './Components/Product/Product'
 
 function App() {
   //function dengan parameter store untuk mengambil state count di counter
@@ -16,8 +17,10 @@ function App() {
       <button onClick={() => dispatch(counterSlice.actions.decrement())}>DECREMENT</button>
       <button onClick={() => dispatch(counterSlice.actions.increaseByNumber(5))}>Increment By 5</button>
       <button onClick={() => dispatch(counterSlice.actions.increaseByNumber(10))}>Increase By 10</button>
-      
+      <hr />
       <Burger />
+      <hr />
+      <Product />
     </div>
   );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './counter'
 import burgerSlice from './burger'
+import productSlice from './product'
 
 const store = configureStore({
     //parameter dengan sebuah objek
     reducer: {
         counter: counterSlice.reducer,
-        burger: burgerSlice.reducer
+        burger: burgerSlice.reducer,
+        product: productSlice.reducer
     }  //1 properti dengan value objek
 })
 
